1e68903a614a29d556e1e2a23aa2206268e154f9
probots-ml <87763103+probots-ml@users.noreply.github.com>
2024-03-26 14:06:46 -0300

    Test colors

--- test.ts
+++ test.ts
@@ -1,2 +1,6 @@
 // aquí va el archivo "tests"; esto no se compilará cuando este paquete se utilice como una extensión.
 probots.rgbOn(ColorLedRGB.Orange);
+basic.pause(1000);
+probots.rgbOff();
+probots.rgbOnByTime(ColorLedRGB.Orange, 2000);
+probots.rgbOnByTime(ColorLedRGB.Cyan,2000);
