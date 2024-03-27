9e4b1411b6cda6b3976fbc04cc9ce48363061f50
probots-ml <87763103+probots-ml@users.noreply.github.com>
2024-03-27 16:37:20 -0300

    Add Laser Blocks.

---
+++ probots-led-laser.ts
@@ -0,0 +1,39 @@
+namespace probots {
+
+    //% block="turn on Laser on %myPort=brickPort"
+    //% block.loc.es="encender Láser en puerto %myPort=brickPort"  
+    //% weight=100
+    //% subcategory="Laser"
+    //% color=#FF1122
+    export function laserOn(myPort: any): void {
+        pins.digitalWritePin(myPort.P0, 1);
+        pins.digitalWritePin(myPort.P1, 0);
+        return
+    }
+
+    //% block="turn Laser on %myPort=brickPort with intensity %intensity"
+    //% block.loc.es="encender Láser en puerto %myPort=brickPort con itensidad %intensity"
+    //% weight=98
+    //% subcategory="Laser"
+    //% color=#FF1122
+    //% intensity.defl=50
+    //% intensity.min=0 intensity.max=100
+    export function laserOnWithPower(myPort: any, intensity: number): void {
+        let intensityMapped = pins.map(intensity, 0, 100, 0, 1024);
+        pins.analogWritePin(getAnalogPin(myPort.P0), intensityMapped);
+        pins.digitalWritePin(myPort.P1, 0);
+        return
+    }
+
+    //% block="turn off Laser on %myPort=brickPort"
+    //% block.loc.es="apagar Láser en puerto %myPort=brickPort"
+    //% weight=97
+    //% subcategory="Laser"
+    //% color=#FF1122
+    export function laserOff(myPort: any): void {
+        pins.digitalWritePin(myPort.P0, 0);
+        pins.digitalWritePin(myPort.P1, 0);
+        return
+    }
+}
+
--- test.ts
+++ test.ts
@@ -1,10 +1,16 @@
 basic.forever(function () {
-    probots.ledOn(probots.brickPort(ConnectionPorts.CON2))
-    basic.pause(500)
-    probots.ledOff(probots.brickPort(ConnectionPorts.CON2))
-    basic.pause(500)
-    probots.ledOnWithPower(probots.brickPort(ConnectionPorts.CON2),50)
-    basic.pause(500)
-    probots.ledOff(probots.brickPort(ConnectionPorts.CON2))
-    basic.pause(500)
+    if (probots.isButtonPressed(probots.brickPort(ConnectionPorts.CON1))) {
+        probots.laserOn(probots.brickPort(ConnectionPorts.CON7))
+        basic.pause(200)
+        probots.laserOff(probots.brickPort(ConnectionPorts.CON7))
+        basic.pause(500)
+    } else {
+        probots.ledOn(probots.brickPort(ConnectionPorts.CON7))
+        basic.pause(500)
+        probots.ledOff(probots.brickPort(ConnectionPorts.CON7))
+        basic.pause(500)
+        probots.ledOnWithPower(probots.brickPort(ConnectionPorts.CON7), 12)
+        basic.pause(500)
+        probots.ledOff(probots.brickPort(ConnectionPorts.CON7))
+    }
 })
