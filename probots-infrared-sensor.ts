08a124319c31012a76bd05cf5cb413601d63e0a3
probots-ml <87763103+probots-ml@users.noreply.github.com>
2024-03-27 19:16:17 -0300

    Add IR sensor blocks.

---
+++ probots-infrared-sensor.ts
@@ -0,0 +1,18 @@
+namespace probots {
+
+    /**
+     * Detects a objects if there is near a 1 centimeter or less.
+     * @param myPort port of connection
+    */
+    //% block="sensor IR detects objects on %myPort=brickPort"
+    //% block.loc.es="sensor IR detecta objeto en puerto %myPort=brickPort"  
+    //% weight=100
+    //% subcategory="IR Sensor"
+    //% color=#A1A5A6
+    export function infraredIsOn(myPort: any): boolean {
+
+        return pins.digitalReadPin(myPort.P0) === 1? true : false;
+        
+    }
+}
+
