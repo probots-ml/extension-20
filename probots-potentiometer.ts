a77eac3a3c5f833ff81a88bc5dfbafa40c0e850c
probots-ml <87763103+probots-ml@users.noreply.github.com>
2024-03-27 18:43:01 -0300

    Add potentiometer adjusts.

--- probots-potentiometer.ts
+++ probots-potentiometer.ts
@@ -2,13 +2,14 @@ namespace probots {
 
     //% block="value of potentiometer in %myPort=brickPort"
     //% block.loc.es="valor del potenciómetro en puerto %myPort=brickPort"  
-    //% jsdoc.loc.en="Return de potentiometer value from 0 to 100"
+    //% jsdoc.loc.en="Return the potentiometer value from 0 to 100"
+    //% jsdoc.loc.es="Devuelve el valor del potenciómetro de 0 a 100"
     //% weight=100
     //% subcategory="Potentiometer"
     //% color=#663377
     export function potentiometer(myPort: any): number {
-        let analogValue = pins.analogReadPin(getAnalogPin(myPort.P0))
-        return pins.map(analogValue, 0, 1023, 0, 100); 
+        let analogValue = pins.analogReadPin(getAnalogPin(myPort.P1))
+        return pins.map(analogValue, 0, 1023, 0, 100);
     }
 
 }
--- test.ts
+++ test.ts
@@ -1,16 +1,3 @@
 basic.forever(function () {
-    if (probots.potentiometer(probots.brickPort(ConnectionPorts.CON1)) == 0) {
-        probots.laserOn(probots.brickPort(ConnectionPorts.CON7))
-        basic.pause(200)
-        probots.laserOff(probots.brickPort(ConnectionPorts.CON7))
-        basic.pause(500)
-    } else {
-        probots.ledOn(probots.brickPort(ConnectionPorts.CON7))
-        basic.pause(500)
-        probots.ledOff(probots.brickPort(ConnectionPorts.CON7))
-        basic.pause(500)
-        probots.ledOnWithPower(probots.brickPort(ConnectionPorts.CON7), 12)
-        basic.pause(500)
-        probots.ledOff(probots.brickPort(ConnectionPorts.CON7))
-    }
+    basic.showNumber(probots.potentiometer(probots.brickPort(ConnectionPorts.CON1)))
 })
