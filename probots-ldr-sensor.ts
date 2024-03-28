4003170e0661b169c0c20246ba85067684963e12
probots-ml <87763103+probots-ml@users.noreply.github.com>
2024-03-28 13:32:57 -0300

    LDR add serial console view.

--- probots-ldr-sensor.ts
+++ probots-ldr-sensor.ts
@@ -3,15 +3,19 @@ namespace probots {
      * Detects the amount of light using LDR sensor, in a range from 0 (no light) to 100 (maximum amount of light).
      * @param myPort port of connection
     */
-    //% block="LDR value on %myPort=brickPort"
+    //% blockId=ldrValue
+    //% block="LDR value on %myPort=brickPort || show in console $showInSerialPort"
+    //% showInSerialPort.defl=false
     //% block.loc.es="valor de LDR en puerto %myPort=brickPort"  
     //% weight=100
     //% subcategory="LDR Sensor"
     //% color=#F93274
-    export function ldrValue(myPort: any): number {
+    export function ldrValue(myPort: any, showInSerialPort: boolean): number {
 
         let ldrVal = pins.analogReadPin(getAnalogPin(myPort.P1));
-
+        if(showInSerialPort){
+            serial.writeLine("LDR " + ldrVal);
+        }
         return pins.map(ldrVal,0,1023,0,100);
     }
 }
