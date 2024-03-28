dd640ea68c241f43d167406b233259f550286412
probots-ml <87763103+probots-ml@users.noreply.github.com>
2024-03-28 17:43:46 -0300

    Add level of soil moisture.

--- probots-soil-mosture.ts
+++ probots-soil-mosture.ts
@@ -1,25 +1,32 @@
 namespace probots {
 
+    /**
+         * Detects a if there is enough humidity in the soil.
+         * @param myPort port of connection
+    */
     //% block="high soil mosture on %myPort=brickPort"
     //% block.loc.es="humedad del suelo alta en puerto %myPort=brickPort"  
     //% weight=100
     //% subcategory="Soil Mosture"
     //% color=#BC21BC
     export function soilMosture(myPort: any): boolean {
-        return pins.digitalReadPin(myPort.P0) == 1 ? false : true;
+        return pins.digitalReadPin(myPort.P0) == 1 ? false : true
     }
 
-/*
-    //% block="level soil mosture on %myPort=brickPort between 0 to 100"
-    //% block.loc.es="nivel de humedad del suelo en puerto %myPort=brickPort entre 0 y 100"  
+
+    /**
+         * Get the level of soil humidity. The value goes from 0 (dry) to 100 (wet).
+         * @param myPort port of connection
+        */
+    //% block="level soil mosture on %myPort=brickPortADC"
+    //% block.loc.es="nivel de humedad del suelo en puerto %myPort=brickPortADC"  
     //% weight=99
     //% subcategory="Soil Mosture"
     //% color=#BC21BC
     export function soilMostureLevel(myPort: any): number {
-        let soilMostureValue = pins.map(pins.analogReadPin(getAnalogPin(myPort.P1)), 0, 1023, 0, 100)
-        serial.writeLine("soil=" + soilMostureValue);
+        let soilMostureValue = pins.map(pins.analogReadPin(getAnalogPin(myPort.P1)), 1023, 0, 0, 100)
+        //serial.writeLine("soil=" + soilMostureValue);
         return soilMostureValue;
     }
-    */
 }
 
