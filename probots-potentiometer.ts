8dd5ba0daefc23b8b6049f887fd035119cffacf8
probots-ml <87763103+probots-ml@users.noreply.github.com>
2024-03-28 15:32:19 -0300

    Add ADC Comm support to potentiometer.

--- probots-potentiometer.ts
+++ probots-potentiometer.ts
@@ -5,7 +5,7 @@ namespace probots {
      * @param myPort port of connection
     */
     //% block="value of potentiometer in %myPort=brickPort"
-    //% block.loc.es="valor del potenciómetro en puerto %myPort=brickPort"  
+    //% block.loc.es="valor del potenciómetro en puerto %myPort=brickPortADC"  
     //% jsdoc.loc.es="Devuelve el valor del potenciómetro de 0 a 100"
     //% weight=100
     //% subcategory="Potentiometer"
--- test.ts
+++ test.ts
@@ -3,7 +3,7 @@ basic.pause(1000)
 probots.ledOff(probots.brickPort(ConnectionPorts.CON3))
 basic.pause(1000)
 basic.forever(function () {
-    if (probots.microphoneDetectSound(soundIntensity.HIGH, probots.brickPort(ConnectionPorts.CON4))) {
+    if (probots.microphoneDetectSound(soundIntensity.MEDIUM, probots.brickPortADC(ADCConnectionPorts.CON4))) {
         probots.ledOn(probots.brickPort(ConnectionPorts.CON3))
     } else {
         probots.ledOff(probots.brickPort(ConnectionPorts.CON3))
