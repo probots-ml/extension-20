791cc6d5b83d88e0c26df0788b4c4f8c0b4c7193
probots-ml <87763103+probots-ml@users.noreply.github.com>
2024-03-29 18:09:57 -0300

    Fix some minors bugs.

--- probots-servo-motor.ts
+++ probots-servo-motor.ts
@@ -7,8 +7,7 @@ namespace probots{
     //% weight=100
     //% subcategory="Servo Motor"
     //% color=#99AA21
-    //% block="Servo $con=conexiones_ret | degrees | %grados"
-    //% degrees.min=0 degrees.max=180
+    //% degrees.min=0 degrees.max=180 degrees.defl=90
     export function servoProbot(myPort: any, degrees: number) {
         return pins.servoWritePin(getAnalogPin(myPort.P0), degrees)
     }
