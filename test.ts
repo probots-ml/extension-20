4cce1790a73dc5046984c7b496e602c89c4d4915
probots-ml <87763103+probots-ml@users.noreply.github.com>
2024-04-24 16:10:53 -0300

    Actualizar test.ts

--- test.ts
+++ test.ts
@@ -1,4 +1,9 @@
-
+let leds_neopixel: probots.Strip = null
+probots.showColor(leds_neopixel, Colors.Violet)
+basic.pause(100)
+probots.showColor(leds_neopixel, Colors.Yellow)
+basic.pause(100)
+basic.pause(100)
 basic.forever(function () {
-   
+    leds_neopixel = probots.newStripNeopixel(probots.brickPort(ConnectionPorts.CON1), 8)
 })
