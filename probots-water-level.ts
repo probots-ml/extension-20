415b857db99436f59621830c139712277023d027
probots-ml <87763103+probots-ml@users.noreply.github.com>
2024-04-25 17:00:36 -0300

    Fix water level name.

--- probots-water-level.ts
+++ probots-water-level.ts
@@ -4,13 +4,13 @@ namespace probots {
      * Read the level of water in a range from 0 (empty water) to 100 (full level).
      * @param myPort port of connection
     */
-    //% blockId=level
-    //% block="Water leve on %myPort=brickPortADC"
+    //% blockId=waterLevel
+    //% block="water level on %myPort=brickPortADC"
     //% block.loc.es="nivel de agua en el puerto %myPort=brickPortADC"
     //% weight=100
     //% subcategory="Water Level"
     //% color=#F93274
-    export function level(myPort: any): number {
+    export function waterLevel(myPort: any): number {
 
         let actualLevel = pins.analogReadPin(getAnalogPin(myPort.P1));
         
