f2494570eefbb04bc295a822d872bf3164d5b314
probots-ml <87763103+probots-ml@users.noreply.github.com>
2024-04-26 14:53:28 -0300

    Add Pasive Buzzer block.

--- probots-buzzer-pasive.ts
+++ probots-buzzer-pasive.ts
@@ -4,14 +4,14 @@
 // This sensor use the pin #6, normaly used by SDA 
 // in the rest of ports
 
-namespace probots{
+namespace probots {
 
     //% block="turn buzzer on on %myPort=brickPort4Pins"
     //% block.loc.es="activar buzzer en puerto %myPort=brickPort4Pins"
     //% weight=100
-    //% subcategory="Active Buzzer"
-    //% color=#A39122
-    export function buzzerActiveOn(myPort: any): void {    
+    //% subcategory="Pasive Buzzer"
+    //% color=#A31298
+    export function buzzerPasiveOn(myPort: any): void {
         pins.digitalWritePin(myPort.P3, 1);
         return
     }
@@ -19,10 +19,10 @@ namespace probots{
     //% block="turn buzzer off on %myPort=brickPort4Pins"
     //% block.loc.es="desactivar buzzer en puerto %myPort=brickPort4Pins"
     //% weight=99
-    //% subcategory="Active Buzzer"
-    //% color=#A39122
-    export function buzzerActiveOff(myPort: any): void {
+    //% subcategory="Pasive Buzzer"
+    //% color=#A31298
+    export function buzzerPasiveOff(myPort: any): void {
         pins.digitalWritePin(myPort.P3, 0);
         return
-    } 
+    }
 }
