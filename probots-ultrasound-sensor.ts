dc4fd013b260facf8ef054c9f4929d14b5118c71
probots-ml <87763103+probots-ml@users.noreply.github.com>
2024-05-08 12:13:10 -0300

    Fix some bugs in Ultrasonic sensor.

--- probots-ultrasound-sensor.ts
+++ probots-ultrasound-sensor.ts
@@ -21,4 +21,26 @@ namespace probots {
         const d = pins.pulseIn(myPort.P0, PulseValue.High, maxCmDistance * 58);
         return Math.idiv(d, 58);
     }
+
+    /**
+     * Get the distance in centimeters from an object. The maximum distance is 5 meters.
+     * @param myPort port of connection
+    */
+    //% block="distance on %myPort=brickPort v2"  
+    //% weight=99
+    //% subcategory="Ultrasonic Sensor"
+    //% color=#F025A8
+    export function ultrasoundDistanceNew(myPort: any): number {
+        // send pulse
+        let maxCmDistance = 500;
+        //pins.setPull(myPort.P1, PinPullMode.PullNone);
+        //pins.digitalWritePin(myPort.P1, 0);
+        //control.waitMicros(2);
+        pins.digitalWritePin(myPort.P1, 0);
+        control.waitMicros(10);
+        pins.digitalWritePin(myPort.P1, 1);
+        // read pulse
+        const d = pins.pulseIn(myPort.P0, PulseValue.High, maxCmDistance * 58);
+        return Math.idiv(d, 58);
+    }
 }
--- test.ts
+++ test.ts
@@ -1,3 +1,4 @@
 basic.forever(function () {
-   
+    serial.writeValue("Distancia", probots.ultrasoundDistanceNew(probots.brickPort(ConnectionPorts.CON1)))
+    basic.pause(100)
 })
