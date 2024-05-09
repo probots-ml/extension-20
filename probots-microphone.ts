8337cb2eec3c78c413d22503f6b8d7ec957ed558
probots-ml <87763103+probots-ml@users.noreply.github.com>
2024-05-09 15:03:12 -0300

    Microphone delta sound function.

--- probots-microphone.ts
+++ probots-microphone.ts
@@ -66,4 +66,48 @@ namespace probots{
         }
     }
 
+
+    /**
+     * Detects deltas in a sound is at least equal to the selected intensity.
+     * @param db select the intensity of the sound
+     * @param myPort port of connection
+    */
+    //% blockId=microphoneDeltaSound
+    //% block="delta sound %db=soundIntensity in port %myPort=brickPortADC"
+    //% db.defl=soundIntensity.LOW
+    //% subcategory="Sound Sensor"
+    //% color=#88DD05
+    //% weight=98
+    export function microphoneDeltaSound(db: soundIntensity, myPort: any): number {
+
+        let getDB = 0;
+        let max = 0;
+        let min = 1024;
+        let delta = 0;
+
+        for (let i = 0; i < 100; i++) {
+            getDB = pins.analogReadPin(getAnalogPin(myPort.P1));
+            max = Math.max(max, getDB);
+            min = Math.min(min, getDB);
+        }
+
+        delta = max - min;
+       
+       return delta;
+    /*
+        if (db == soundIntensity.HIGH && delta >= 20) {
+            return true;
+        }
+        else if (db == soundIntensity.MEDIUM && initialValue >= 950) {
+            return true;
+        }
+        else if (db == soundIntensity.LOW && initialValue >= 900) {
+            return true;
+        }
+        else {
+            return false;
+        }
+        */
+    }
+
 }
