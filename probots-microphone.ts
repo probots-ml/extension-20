7b17259a43e5cb4721e1f4171f0fde5e983ad3a8
probots-ml <87763103+probots-ml@users.noreply.github.com>
2024-05-09 14:26:34 -0300

    Add intensities to a microphoone.

--- probots-microphone.ts
+++ probots-microphone.ts
@@ -25,4 +25,45 @@ namespace probots{
         getDB = pins.map(getDB, 0, 1023, 0, 100);
         return getDB;   
     }
+
+
+    /**
+     * Detects if a sound is at least equal to the selected intensity.
+     * @param db select the intensity of the sound
+     * @param myPort port of connection
+    */
+    //% blockId=microphoneDetectSound
+    //% block="there is a sound %db=soundIntensity in port %myPort=brickPortADC"
+    //% db.defl=soundIntensity.MEDIUM
+    //% subcategory="Sound Sensor"
+    //% color=#88DD05
+    //% weight=99
+    export function microphoneDetectSound(db: soundIntensity, myPort: any): boolean {
+
+        let initialValue = 0;
+        let getDB = 0;
+
+        for (let i = 0; i < 100; i++) {
+            getDB = pins.analogReadPin(getAnalogPin(myPort.P1));
+            if (getDB > initialValue) {
+                initialValue = getDB;
+            }
+        }
+
+        //serial.writeLine("db=" + initialValue);
+
+        if (db == soundIntensity.HIGH && initialValue >= 1000) {
+            return true;
+        }
+        else if (db == soundIntensity.MEDIUM && initialValue >= 950) {
+            return true;
+        }
+        else if (db == soundIntensity.LOW && initialValue >= 900) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
 }
