f645693f2a2c1f4858f5692cfa6476880ea0371a
probots-ml <87763103+probots-ml@users.noreply.github.com>
2024-05-10 15:50:19 -0300

    Fix serial port plot in LDR.

--- probots-ldr-sensor.ts
+++ probots-ldr-sensor.ts
@@ -4,18 +4,14 @@ namespace probots {
      * @param myPort port of connection
     */
     //% blockId=ldrValue
-    //% block="LDR value on %myPort=brickPortADC || show in console $showInSerialPort"
-    //% showInSerialPort.defl=false
-    //% block.loc.es="valor de LDR en puerto %myPort=brickPortADC"
+    //% block="LDR value on %myPort=brickPortADC"
     //% weight=100
     //% subcategory="LDR Sensor"
     //% color=#F93274
-    export function ldrValue(myPort: any, showInSerialPort: boolean): number {
+    export function ldrValue(myPort: any): number {
 
         let ldrVal = pins.analogReadPin(getAnalogPin(myPort.P1));
-        if(showInSerialPort){
-            serial.writeLine("LDR " + ldrVal);
-        }
+        
         return pins.map(ldrVal,0,1023,0,100);
     }
 }
