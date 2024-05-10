0dd7ef28c0954cc5ed1a6730a9377e15d2801034
probots-ml <87763103+probots-ml@users.noreply.github.com>
2024-05-10 11:05:23 -0300

    Fix microphone blocks.

--- probots-microphone.ts
+++ probots-microphone.ts
@@ -42,25 +42,15 @@ namespace probots{
     //% weight=99
     export function microphoneDetectSound(db: soundIntensity, myPort: any): boolean {
 
-        let initialValue = 0;
-        let getDB = 0;
+        let deltaSound = microphoneDeltaSound(myPort);
 
-        for (let i = 0; i < 100; i++) {
-            getDB = pins.analogReadPin(getAnalogPin(myPort.P1));
-            if (getDB > initialValue) {
-                initialValue = getDB;
-            }
-        }
-
-        //serial.writeLine("db=" + initialValue);
-
-        if (db == soundIntensity.HIGH && initialValue >= 1000) {
+        if (db == soundIntensity.HIGH && deltaSound >= 25) {
             return true;
         }
-        else if (db == soundIntensity.MEDIUM && initialValue >= 950) {
+        else if (db == soundIntensity.MEDIUM && deltaSound >= 15) {
             return true;
         }
-        else if (db == soundIntensity.LOW && initialValue >= 900) {
+        else if (db == soundIntensity.LOW && deltaSound >= 5) {
             return true;
         }
         else {
@@ -70,17 +60,15 @@ namespace probots{
 
 
     /**
-     * Detects deltas in a sound is at least equal to the selected intensity.
-     * @param db select the intensity of the sound
+     * Read the intensity of a sound using the microphone, using a range from 0 to 100.
      * @param myPort port of connection
     */
     //% blockId=microphoneDeltaSound
-    //% block="delta sound %db=soundIntensity in port %myPort=brickPortADC"
-    //% db.defl=soundIntensity.LOW
+    //% block="get sound in port %myPort=brickPortADC"
     //% subcategory="Sound Sensor"
     //% color=#88DD05
     //% weight=98
-    export function microphoneDeltaSound(db: soundIntensity, myPort: any): number {
+    export function microphoneDeltaSound(myPort: any): number {
 
         let getDB = 0;
         let max = 0;
@@ -94,26 +82,11 @@ namespace probots{
         }
 
         delta = 0.5 * (max + lastMax) - 0.5 * (min + lastMin);
-       
-
+    
         lastMax = max;
         lastMin = min;
 
-       return delta;
-    /*
-        if (db == soundIntensity.HIGH && delta >= 20) {
-            return true;
-        }
-        else if (db == soundIntensity.MEDIUM && initialValue >= 950) {
-            return true;
-        }
-        else if (db == soundIntensity.LOW && initialValue >= 900) {
-            return true;
-        }
-        else {
-            return false;
-        }
-        */
+        return pins.map(delta, 0 , 1023, 0 , 100);   
     }
 
 }
--- test.ts
+++ test.ts
@@ -1,4 +1,4 @@
-probots.rgbPortOn(probots.brickPort4Pins(ConnectionPorts4Pins.CON7), ColorLedRGB.Orange)
+
 
 basic.forever(function () {
    
