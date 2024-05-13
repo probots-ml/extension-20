29aff81710cb5e0ab2024df8ed3f0a9f15f71646
probots-ml <87763103+probots-ml@users.noreply.github.com>
2024-05-13 13:55:27 -0300

    Update Tones table.

--- probots-tones.ts
+++ probots-tones.ts
@@ -1,17 +1,29 @@
-// Parameters for...
+// Tone Table
 
 
 enum TONE {
+    //% block="A4"
     A = 440,
+    //% block="A#4"
     A_SHARP = 466,
+    //% block="B4"
     B = 494,
+    //% block="C4"
     C = 523,
+    //% block="C#4"
     C_SHARP = 554,
+    //% block="D4"
     D = 587,
+    //% block="D#4"
     D_SHARP = 622,
+    //% block="E4"
     E = 659,
+    //% block="F4"
     F = 698,
+    //% block="F#4"
     F_SHARP = 740,
+    //% block="G4"
     G = 784,
+    //% block="G#4"
     G_SHARP = 830
-}// Añade tu código aquí
+}
