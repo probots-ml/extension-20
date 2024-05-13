58ae2b835d44f2d3fcb7da43321c2c636378371a
probots-ml <87763103+probots-ml@users.noreply.github.com>
2024-05-13 15:39:30 -0300

    Fix Potentiometer led.enable

--- probots-potentiometer.ts
+++ probots-potentiometer.ts
@@ -12,7 +12,9 @@ namespace probots {
     //% subcategory="Potentiometer"
     //% color=#663377
     export function potentiometer(myPort: any): number {
+        led.enable(false);
         let analogValue = pins.analogReadPin(getAnalogPin(myPort.P1))
+        led.enable(true);
         return pins.map(analogValue, 0, 1023, 0, 100);
     }
 
