8a99641b2d4fdcabb641c09e50fc6594fc82e5bc
probots-ml <87763103+probots-ml@users.noreply.github.com>
2024-05-13 14:07:14 -0300

    Fixed name of tone tables.

--- probots-tones.ts
+++ probots-tones.ts
@@ -2,28 +2,28 @@
 
 
 enum TONE {
-    //% block="A4"
+    //% block=A4
     A = 440,
-    //% block="A#4"
+    //% block=A#4
     A_SHARP = 466,
-    //% block="B4"
+    //% block=B4
     B = 494,
-    //% block="C4"
+    //% block=C4
     C = 523,
-    //% block="C#4"
+    //% block=C#4
     C_SHARP = 554,
-    //% block="D4"
+    //% block=D4
     D = 587,
-    //% block="D#4"
+    //% block=D#4
     D_SHARP = 622,
-    //% block="E4"
+    //% block=E4
     E = 659,
-    //% block="F4"
+    //% block=F4
     F = 698,
-    //% block="F#4"
+    //% block=F#4
     F_SHARP = 740,
-    //% block="G4"
+    //% block=G4
     G = 784,
-    //% block="G#4"
+    //% block=G#4
     G_SHARP = 830
 }
