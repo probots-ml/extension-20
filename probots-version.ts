1e8b5d7c80ac9cc59c73d5ade8224c1ad368cbe2
probots-ml <87763103+probots-ml@users.noreply.github.com>
2024-05-17 12:39:39 -0300

    Change Version, now in Miscellaneous subcaterory.

--- probots-version.ts
+++ probots-version.ts
@@ -2,7 +2,7 @@ namespace probots {
 
     //% block="VER 0.35.1"
     //% weight=100
-    //% subcategory="Version"
+    //% subcategory="Miscellaneous"
     //% color=#663377
     export function getActualVersion(): string {
         return "0.35.1";
