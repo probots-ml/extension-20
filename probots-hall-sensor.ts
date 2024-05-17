8719ec4a49711ba8c87b92b255bbd34ba87e315e
probots-ml <87763103+probots-ml@users.noreply.github.com>
2024-05-17 12:20:10 -0300

    Add Hall value and add Version block.

--- probots-hall-sensor.ts
+++ probots-hall-sensor.ts
@@ -19,5 +19,16 @@ namespace probots {
             return false;
         }
     }
+
+    //% block="hall value on %myPort=brickPort"
+    //% weight=99
+    //% subcategory="Hall Sensor"
+    //% color=#99AA21
+    export function hallSensorValue(myPort: any): number {
+        led.enable(false);
+        let reading = pins.digitalReadPin(myPort.P0);
+        led.enable(true);
+        return reading;
+    }
 }
 
---
+++ probots-version.ts
@@ -0,0 +1,11 @@
+namespace probots {
+
+    //% block="VER 0.37.8"
+    //% weight=100
+    //% subcategory="VERSION"
+    //% color=#663377
+    export function getActualVersion(): string {
+        return "0.37.8";
+    }
+
+}
