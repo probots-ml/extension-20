6d74fb647b35460affd06240029b5f556c1b94d1
probots-ml <87763103+probots-ml@users.noreply.github.com>
2024-05-20 12:20:43 -0300

    Add PIR Block

---
+++ probots-pir.ts
@@ -0,0 +1,23 @@
+namespace probots {
+
+    //% block="activity was detected on %myPort=brickPort"
+    //% weight=100
+    //% subcategory="PIR Sensor"
+    //% color=#663377
+    export function pirSensor(myPort: any): boolean {
+
+        led.enable(false);
+
+        let reading = pins.digitalReadPin(myPort.P0);
+
+        led.enable(true);
+
+        if (reading == 1) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+}
