b2f8e286ef303c22f1613c0fd6a23dd4fd3f4e33
probots-ml <87763103+probots-ml@users.noreply.github.com>
2024-05-20 12:37:09 -0300

    Fix Buzzer Active to new pin electronic desing.

--- probots-buzzer-active.ts
+++ probots-buzzer-active.ts
@@ -4,23 +4,26 @@
 // This sensor use the pin #6, normaly used by SDA 
 // in the rest of ports
 
+// 2024-05-20
+// The pins connection was changed to P0
+
 namespace probots{
 
-    //% block="turn buzzer on on %myPort=brickPort4Pins"
+    //% block="turn buzzer on on %myPort=brickPort"
     //% weight=100
     //% subcategory="Active Buzzer"
     //% color=#A39122
     export function buzzerActiveOn(myPort: any): void {    
-        pins.digitalWritePin(myPort.P3, 1);
+        pins.digitalWritePin(myPort.P0, 1);
         return
     }
 
-    //% block="turn buzzer off on %myPort=brickPort4Pins"
+    //% block="turn buzzer off on %myPort=brickPort"
     //% weight=99
     //% subcategory="Active Buzzer"
     //% color=#A39122
     export function buzzerActiveOff(myPort: any): void {
-        pins.digitalWritePin(myPort.P3, 0);
+        pins.digitalWritePin(myPort.P0, 0);
         return
     } 
 }
