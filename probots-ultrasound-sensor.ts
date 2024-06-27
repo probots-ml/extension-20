187becca101fa5f30733ee1a67211f1598d09497
probots-ml <87763103+probots-ml@users.noreply.github.com>
2024-06-27 13:29:26 -0300

    Fix bux ultrasonic

--- probots-ultrasound-sensor.ts
+++ probots-ultrasound-sensor.ts
@@ -11,24 +11,27 @@ namespace probots {
     //% subcategory="Ultrasonic Sensor"
     //% color=#F025A8
     export function ultrasoundDistance(myPort: any): number {
+        
         led.enable(false);
 
-        if( input.runningTime() > _lastTimeOfReading)
+        if(input.runningTime() > _lastTimeOfReading)
         {       
             // send pulse
             let maxCmDistance = 400;
             pins.digitalWritePin(myPort.P1, 1);
-            control.waitMicros(10);
+            control.waitMicros(20);
             pins.digitalWritePin(myPort.P1, 0);
 
             // read pulse
             const d = pins.pulseIn(myPort.P0, PulseValue.High, maxCmDistance * 58);
 
-            led.enable(true);
+            
             _lastDistance =  Math.idiv(d, 58);
 
             _lastTimeOfReading = input.runningTime() + 100;
         }
+
+        led.enable(true);
 
         return _lastDistance;
     }
