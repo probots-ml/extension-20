0a43153217c61cc3722a49c52b884ca894e5f83e
probots-ml <87763103+probots-ml@users.noreply.github.com>
2024-06-28 15:58:05 -0300

    set pin 15 and 11 to low

--- probots-microbit-brick-v1.ts
+++ probots-microbit-brick-v1.ts
@@ -63,15 +63,7 @@ enum ConnectionPortsAnalogPitch {
 
 namespace probots{
 
-    pins.digitalWritePin(DigitalPin.P8, 0);
-    pins.digitalWritePin(DigitalPin.P0, 0);
-    serial.writeValue("pin8", pins.digitalReadPin(DigitalPin.P8));
-    serial.writeValue("pin0", pins.digitalReadPin(DigitalPin.P0));
-    
-    pins.digitalWritePin(DigitalPin.P15, 0);
-    pins.digitalWritePin(DigitalPin.P11, 0);
-    serial.writeValue("pin15", pins.digitalReadPin(DigitalPin.P15));
-    serial.writeValue("pin11", pins.digitalReadPin(DigitalPin.P11));
+   
 
 /**
  * Description of any pinout connection over each port.
@@ -199,4 +191,15 @@ let pinOut: any = {
         }
     }
    
+
+    pins.digitalWritePin(DigitalPin.P8, 0);
+    pins.digitalWritePin(DigitalPin.P0, 0);
+    serial.writeValue("pin8", pins.digitalReadPin(DigitalPin.P8));
+    serial.writeValue("pin0", pins.digitalReadPin(DigitalPin.P0));
+
+    pins.digitalWritePin(DigitalPin.P15, 0);
+    pins.digitalWritePin(DigitalPin.P11, 0);
+    serial.writeValue("pin15", pins.digitalReadPin(DigitalPin.P15));
+    serial.writeValue("pin11", pins.digitalReadPin(DigitalPin.P11));
+    
 }
