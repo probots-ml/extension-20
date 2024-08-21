9532ba0717306456e1f31ec68098401a7d70e9d8
probots-ml <87763103+probots-ml@users.noreply.github.com>
2024-08-21 08:42:11 -0300

    Button: set pull up function for digital input pin.

--- probots-push-button.ts
+++ probots-push-button.ts
@@ -1,14 +1,14 @@
 namespace probots {
-
+    
     //% block="button was pressed on %myPort=brickPort"
-    //% block.loc.es="botón presionado en puerto %myPort=brickPort"  
     //% weight=100
     //% subcategory="Push Button"
     //% color=#663377
     export function isButtonPressed(myPort: any): boolean {
     
         led.enable(false);
-    
+        
+        pins.setPull(myPort.P0, PinPullMode.PullUp);
         let reading = pins.digitalReadPin(myPort.P0);
 
         led.enable(true);
--- test.ts
+++ test.ts
@@ -1,8 +1,10 @@
+
+
 basic.forever(function () {
-    if (probots.capacitiveSensor(probots.brickPort(ConnectionPorts.CON2))) {
+    if (probots.isButtonPressed(probots.brickPort(ConnectionPorts.CON2))) {
         serial.writeValue("x", 1)
     } else {
         serial.writeValue("x", 0)
-        basic.pause(100)
+        
     }
 })
