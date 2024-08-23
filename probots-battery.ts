42027bb6cf8241d894f9204f611928d632a24cd7
probots-ml <87763103+probots-ml@users.noreply.github.com>
2024-08-23 18:28:13 -0300

    While in charge battery to not allow other functions

--- probots-battery.ts
+++ probots-battery.ts
@@ -16,50 +16,49 @@ namespace probots {
     //% subcategory="Battery"
     //% color=#EE1111
     export function checkBatteryCharge()  {
-        if (showStatusCharge == 1) 
+        while (!statusWasShowed)
         {
-            pins.setPull(DigitalPin.P5, PinPullMode.PullUp)
-            if (pins.digitalReadPin(DigitalPin.P5) == 0) {
-                isCharged = 0
+            if (showStatusCharge == 1) {
+                pins.setPull(DigitalPin.P5, PinPullMode.PullUp)
+                if (pins.digitalReadPin(DigitalPin.P5) == 0) {
+                    isCharged = 0
+                } else {
+                    isCharged = 1
+                }
+                if (0 == isCharged) {
+                    led.enable(true)
+                    basic.showLeds(`
+                . . # . .
+                . # . # .
+                . # . # .
+                . # . # .
+                . # # # .
+                `)
+                } else if (1 == isCharged) {
+                    led.enable(true)
+                    basic.showLeds(`
+                . . # . .
+                . # # # .
+                . # # # .
+                . # # # .
+                . # # # .
+                `)
+                }
             } else {
-                isCharged = 1
-            }
-            if (0 == isCharged) {
                 led.enable(true)
-                basic.showLeds(`
-                . . # . .
-                . # . # .
-                . # . # .
-                . # . # .
-                . # # # .
-                `)
-            } else if (1 == isCharged) {
-                led.enable(true)
-                basic.showLeds(`
-                . . # . .
-                . # # # .
-                . # # # .
-                . # # # .
-                . # # # .
-                `)
-            }
-        } else {
-            led.enable(true)
-            if (!statusWasShowed)
-            {
-                statusWasShowed = true
+                if (!statusWasShowed) {
+                    statusWasShowed = true
 
-                basic.showLeds(`
+                    basic.showLeds(`
                 . . . . .
                 . . . . .
                 . . . . .
                 . . . . .
                 . . . . .
                 `)
+                }
             }
-            
-        }
-        
+        }     
     }
 
 }
