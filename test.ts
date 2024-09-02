98ffd2d2ae9a3e5a6ec117ed2389a40feab379b2
probots-ml <87763103+probots-ml@users.noreply.github.com>
2024-09-02 11:58:30 -0300

    Add probots.checkBatteryCharge() on forever

--- test.ts
+++ test.ts
@@ -1,6 +1,8 @@
-basic.pause(1000)
 basic.forever(function () {
-    serial.writeValue("h", probots.ambientHumidity(probots.brickPort(ConnectionPorts.CON2)))
-    serial.writeValue("t", probots.ambientTemperature(probots.brickPort(ConnectionPorts.CON2), tempType.celsius))
-    basic.pause(200)
+    probots.checkBatteryCharge()
+    if (probots.soilMosture(probots.brickPortADC(ADCConnectionPorts.CON1), probots.potentiometer(probots.brickPortADC(ADCConnectionPorts.CON2)))) {
+        probots.ledOn(probots.brickPort(ConnectionPorts.CON3))
+    } else {
+        probots.ledOff(probots.brickPort(ConnectionPorts.CON3))
+    }
 })
