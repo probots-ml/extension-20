14b037c9d2336352202e5c78838971a7403e1e3d
probots-ml <87763103+probots-ml@users.noreply.github.com>
2024-09-03 10:32:23 -0300

    Add direct analog read of potentiometer.

--- probots-potentiometer.ts
+++ probots-potentiometer.ts
@@ -13,7 +13,23 @@ namespace probots {
         led.enable(false);
         let analogValue = pins.analogReadPin(getAnalogPin(myPort.P1))
         //led.enable(true);
+
+        
         return pins.map(analogValue, 0, 1023, 100, 0);
     }
 
+    /**
+     * Get a value of analog reading of potentiometer. The range of reading could be between 0 from 1023.
+     * @param myPort port of connection
+    */
+    //%blockId=potentiometer
+    //% block="value of potentiometer in %myPort=brickPortADC"
+    //% weight=99
+    //% subcategory="Potentiometer"
+    //% color=#663377
+    export function potentiometerAnalogRead(myPort: any): number {
+        led.enable(false);
+        let analogValue = pins.analogReadPin(getAnalogPin(myPort.P1))
+        return analogValue;
+    }
 }
