2ce09f8886433fa122ddba82bb54160ec20bca86
probots-ml <87763103+probots-ml@users.noreply.github.com>
2024-09-20 14:54:24 -0300

    Color sensor JoyPi

--- test.ts
+++ test.ts
@@ -1,6 +1,4 @@
-probots.startAndInitRGBSensor()
+
 basic.forever(function () {
-    basic.pause(100)
-    serial.writeValue("d", probots.ultrasoundDistance(probots.brickPort(ConnectionPorts.CON3)))
-    serial.writeValue("r", probots.getSensorData(RGB.RED))
+    
 })
